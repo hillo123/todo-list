573f5c8ce360bce8758e6808b87929dd5cf91e9f
Guillermo <guillermozayas2002@gmail.com>
2024-01-30 17:44:03 +0100

    adding getAllData function

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TodoModel } from '../model/todo-model';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
+  private uri: string =
+    'https://65b7d94846324d531d55a322.mockapi.io/api/to-do/list';
 
-  constructor() { }
+  getAllData(): Observable<TodoModel[]> {
+    return this.http.get<TodoModel[]>(this.uri);
+  }
 }
