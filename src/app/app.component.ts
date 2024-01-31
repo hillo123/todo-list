bc68b5a694e5d0559591881070bb00d648d01a44
Guillermo <guillermozayas2002@gmail.com>
2024-02-01 00:54:57 +0100

    functions created to fetchdata, edit, modify and delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { TodoModel } from './model/todo-model';
+import { PopupService } from './services/popup.service';
+import { BehaviorSubject, Subscription, toArray } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo-list';
+  inputText = '';
+
+  constructor(
+    public apiService: ApiService,
+    public popupService: PopupService
+  ) {}
+
+  ngOnInit(): void {
+    this.popupService.isOpen$.subscribe((nuevoValor) => {
+      this.fetchData();
+    });
+  }
+
+  ngOnDestroy() {
+    console.log('enb destroy');
+  }
+
+  fetchData() {
+    this.apiService.getAllData().subscribe({
+      next: (data) => {
+        this.apiService.dataTask = data;
+        console.log('fetch data');
+        //TODO handle errors
+      },
+    });
+  }
+
+  deleteTaskBtn(id: number) {
+    console.log(id);
+    this.apiService.deleteTask(id).subscribe({
+      next: (resp) => {
+        console.log(resp);
+        this.fetchData();
+        //TODO handle errors
+      },
+    });
+  }
+
+  newTaskBtn(inputText: string) {
+    this.inputText = '';
+    console.log(inputText);
+    this.apiService.addTask(inputText).subscribe({
+      next: (resp) => {
+        console.log(resp);
+        this.fetchData();
+        //TODO handle errors
+      },
+    });
+  }
 }
