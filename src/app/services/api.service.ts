dee0b63c40d36c93ac1eca2d8603dec14b9005f0
Guillermo <guillermozayas2002@gmail.com>
2024-01-31 22:41:29 +0100

    creating the Get route in apiservice

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TodoModel } from '../model/todo-model';
+import { BehaviorSubject, Subscription, toArray } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  public dataTask?: TodoModel[];
+
   constructor(private http: HttpClient) {}
-  private uri: string =
+  private readonly API_MOCK_ENDPOINT: string =
     'https://65b7d94846324d531d55a322.mockapi.io/api/to-do/list';
 
+  // route to fetch entire data
   getAllData(): Observable<TodoModel[]> {
-    return this.http.get<TodoModel[]>(this.uri);
+    return this.http.get<TodoModel[]>(this.API_MOCK_ENDPOINT);
   }
 }
