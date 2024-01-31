5cf1f61dabbf49b3996e69e5f69465d363b666d6
Guillermo <guillermozayas2002@gmail.com>
2024-01-31 22:43:20 +0100

    adding delete , add and modify route in Apiservice

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -17,4 +17,22 @@ export class ApiService {
   getAllData(): Observable<TodoModel[]> {
     return this.http.get<TodoModel[]>(this.API_MOCK_ENDPOINT);
   }
+
+  // route to delete one task
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete(`${this.API_MOCK_ENDPOINT}/${id}`);
+  }
+
+  // route to create new task
+  addTask(data: string) {
+    console.log(data);
+    const body = { task: data };
+    return this.http.post(`${this.API_MOCK_ENDPOINT}`, body);
+  }
+
+  // route to modify one task
+  modifyTask(id: number, data: string): Observable<any> {
+    const body = { task: data };
+    return this.http.put<TodoModel>(`${this.API_MOCK_ENDPOINT}/${id}`, body);
+  }
 }
