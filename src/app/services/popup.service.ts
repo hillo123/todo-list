704884aac94996a948abe6dcbaf517736ea927d0
Guillermo <guillermozayas2002@gmail.com>
2024-01-31 23:20:19 +0100

    creating the popup logic when its open and close (observer)

--- src/app/services/popup.service.ts
+++ src/app/services/popup.service.ts
@@ -3,4 +3,21 @@ import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class PopupService {}
+export class PopupService {
+  dataId: number = 0;
+  dataTask: string = '';
+  isOpenPopup = new BehaviorSubject<boolean>(false);
+  //observers to  use it in main component to know  when should be open or close the Popup
+  isOpen$ = this.isOpenPopup.asObservable();
+
+  //this the logic to edit a task
+  openPopup(id: number, dataTask: string) {
+    this.dataId = id;
+    this.dataTask = dataTask;
+    this.isOpenPopup.next(true);
+  }
+
+  closePopup() {
+    this.isOpenPopup.next(false);
+  }
+}
