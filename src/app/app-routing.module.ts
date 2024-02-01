96ab2b7a7bca3d2b8094ce5c428e2ecae15d2d56
Guillermo <guillermozayas2002@gmail.com>
2024-02-01 20:27:38 +0100

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditComponent } from './components/edit/edit.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
-const routes: Routes = [{ path: 'edit', component: EditComponent }];
+const routes: Routes = [
+  { path: '', component: TodolistComponent },
+  { path: 'edit', component: EditComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
