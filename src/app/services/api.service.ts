99c9741aff213aaf7c62886d25488ccf59100096
Guillermo <guillermozayas2002@gmail.com>
2024-02-01 11:50:12 +0100

    adding a handler errors

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 import { TodoModel } from '../model/todo-model';
-import { BehaviorSubject, Subscription, toArray } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -15,24 +15,41 @@ export class ApiService {
 
   // route to fetch entire data
   getAllData(): Observable<TodoModel[]> {
-    return this.http.get<TodoModel[]>(this.API_MOCK_ENDPOINT);
+    return this.http
+      .get<TodoModel[]>(this.API_MOCK_ENDPOINT)
+      .pipe(catchError(this.handleError));
   }
 
   // route to delete one task
   deleteTask(id: number): Observable<any> {
-    return this.http.delete(`${this.API_MOCK_ENDPOINT}/${id}`);
+    return this.http
+      .delete(`${this.API_MOCK_ENDPOINT}/${id}`)
+      .pipe(catchError(this.handleError));
   }
 
   // route to create new task
   addTask(data: string) {
     console.log(data);
     const body = { task: data };
-    return this.http.post(`${this.API_MOCK_ENDPOINT}`, body);
+    return this.http
+      .post(`${this.API_MOCK_ENDPOINT}`, body)
+      .pipe(catchError(this.handleError));
   }
 
   // route to modify one task
   modifyTask(id: number, data: string): Observable<any> {
     const body = { task: data };
-    return this.http.put<TodoModel>(`${this.API_MOCK_ENDPOINT}/${id}`, body);
+    return this.http
+      .put<TodoModel>(`${this.API_MOCK_ENDPOINT}/${id}`, body)
+      .pipe(catchError(this.handleError));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('we have an error', error.error);
+    } else {
+      console.error('Backend return an error', error.status, error.error);
+    }
+    return throwError(() => new Error('Try again'));
   }
 }
