dbe4b7731a65ce662e37d19e98c1a727002d1edd
Guillermo <guillermozayas2002@gmail.com>
2024-02-02 00:25:54 +0100

    adding imports of angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,31 @@ import { FormsModule } from '@angular/forms';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import { TodolistComponent } from './todolist/todolist.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, EditComponent, SignupComponent, SigninComponent, TodolistComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    EditComponent,
+    SignupComponent,
+    SigninComponent,
+    TodolistComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
