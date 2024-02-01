68e637207d76dddc8c834cf3fe48679492ab01b0
Guillermo <guillermozayas2002@gmail.com>
2024-02-01 11:42:30 +0100

    cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
-import { TodoModel } from './model/todo-model';
 import { PopupService } from './services/popup.service';
-import { BehaviorSubject, Subscription, toArray } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'todo-list';
   inputText = '';
 
   constructor(
@@ -18,22 +16,17 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.popupService.isOpen$.subscribe((nuevoValor) => {
+    this.popupService.isOpen$.subscribe(() => {
       this.fetchData();
     });
-  }
-
-  ngOnDestroy() {
-    console.log('enb destroy');
   }
 
   fetchData() {
     this.apiService.getAllData().subscribe({
       next: (data) => {
         this.apiService.dataTask = data;
-        console.log('fetch data');
-        //TODO handle errors
       },
+      error: (e) => console.error(e),
     });
   }
 
@@ -41,10 +34,9 @@ export class AppComponent implements OnInit {
     console.log(id);
     this.apiService.deleteTask(id).subscribe({
       next: (resp) => {
-        console.log(resp);
         this.fetchData();
-        //TODO handle errors
       },
+      error: (e) => console.error(e),
     });
   }
 
@@ -53,10 +45,9 @@ export class AppComponent implements OnInit {
     console.log(inputText);
     this.apiService.addTask(inputText).subscribe({
       next: (resp) => {
-        console.log(resp);
         this.fetchData();
-        //TODO handle errors
       },
+      error: (e) => console.error(e),
     });
   }
 }
