236c7a8c3da2a154b540d4adc1ee06c832d7c95f
Guillermo <guillermozayas2002@gmail.com>
2024-02-02 00:27:16 +0100

    chaging the logic auth  to firebase

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
-import { UserService } from '../services/user.service';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css'],
 })
-export class SigninComponent {
-  email?: string;
-  password?: string;
+export class SigninComponent implements OnInit {
+  @Output() formData: EventEmitter<{
+    email: string;
+    password: string;
+  }> = new EventEmitter();
+  form?: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
-  constructor(public userService: UserService) {}
-
-  signInUser() {
-    const user = { email: this.email, password: this.password };
-    this.userService.fetchUser(user).subscribe((data) => {
-      console.log(data);
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
     });
+  }
+
+  get email() {
+    return this.form?.get('email');
+  }
+
+  get password() {
+    return this.form?.get('password');
+  }
+
+  onSubmit() {
+    this.formData.emit(this.form?.value);
   }
 }
