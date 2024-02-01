ce4d2b240b9231fb726e9c1c1246d5b145d559bf
Guillermo <guillermozayas2002@gmail.com>
2024-02-02 00:26:55 +0100

    changing  method of auth to firebase

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,18 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from '@angular/fire/auth';
+import { LoginData } from '../model/login-model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private auth: Auth) {}
 
-  fetchUser(user: any): Observable<any> {
-    return this.httpClient.post('https://reqres.in/api/login', user);
+  fetchUser({ email, password }: LoginData) {
+    return signInWithEmailAndPassword(this.auth, email, password);
   }
 
-  addNewUser(user: any): Observable<any> {
-    return this.httpClient.post('https://reqres.in/api/register', user);
+  addNewUser({ email, password }: LoginData) {
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
+  logout() {
+    return signOut(this.auth);
   }
 }
