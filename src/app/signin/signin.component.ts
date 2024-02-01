03ce42538c39a21505ea7702d2e3c38af69202f2
Guillermo <guillermozayas2002@gmail.com>
2024-02-01 20:34:34 +0100

    adding style to the sign In component

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css'],
 })
-export class SigninComponent {}
+export class SigninComponent {
+  email?: string;
+  password?: string;
+
+  signInUser() {
+    console.log(this.email);
+    console.log(this.password);
+  }
+}
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
 
+  constructor() {}
+
+  signUp() {
+    console.log(this.email);
+    console.log(this.password);
+  }
 }
