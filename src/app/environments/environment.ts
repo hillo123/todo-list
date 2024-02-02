444dd3f47fdb0113d258a0483b025bf0e286eb86
Guillermo <guillermozayas2002@gmail.com>
2024-02-02 06:25:09 +0100

    adding edit list and environment of firebase

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDjGOZIRokDG_GN1rSJ_924vMSM57Sp0Dg',
+    authDomain: 'todotask-fireauth.firebaseapp.com',
+    projectId: 'todotask-fireauth',
+    storageBucket: 'todotask-fireauth.appspot.com',
+    messagingSenderId: '320136058332',
+    appId: '1:320136058332:web:bcf196b3187628590dab19',
+    measurementId: 'G-PVX0N2C97J',
+  },
+};
